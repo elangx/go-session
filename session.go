eeba23150ae437db158279e0ff37bfadfabeff03
yangxian <yangxian@didichuxing.com>
2017-09-27 16:38:49 +0800

    fix

--- session.go
+++ session.go
@@ -41,7 +41,7 @@ func (p *SessionMgr) Set(w http.ResponseWriter, key, val interface{}) {
 	cookie := &http.Cookie{
 		Name:    key.(string),
 		Value:   val.(string),
-		Expires: time.Now().Add(p.maxTime.(time.Duration)),
+		Expires: time.Now().Add(p.mMaxTime.(time.Duration)),
 	}
 
 	p.mLock.Lock()
