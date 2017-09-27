7a6bef1b393f17782bedb61b8bb83d7c446dd0f3
yangxian <yangxian@didichuxing.com>
2017-09-27 20:35:05 +0800

    add header cookie

--- session.go
+++ session.go
@@ -7,20 +7,43 @@ import (
 	"time"
 )
 
-const COOKIE_LENGTH = 64
+const KEY_LENGTH = 64
 
 type SessionMgr struct {
-	mSessions   map[string]*Session
-	mCookieName string
-	mLock       sync.RWMutex
-	mMaxTime    int64
+	mSessions map[string]*Session
+	mKeyName  string
+	mLock     sync.RWMutex
+	mMaxTime  int64
+	store     *Store
+}
+
+func NewCookieSessionMgr(keyName string, maxTime int64) *SessionMgr {
+	mgr := &SessionMgr{
+		mKeyName:  keyName,
+		mMaxTime:  maxTime,
+		mSessions: make(map[string]*Session),
+		store:     &CookieStore{KeyName: keyName, ExpTime: maxTime},
+	}
+	mgr.GC()
+	return mgr
+}
+
+func NewHeaderSessionMgr(keyName string, maxTime int64) *SessionMgr {
+	mgr := &SessionMgr{
+		mKeyName:  keyName,
+		mMaxTime:  maxTime,
+		mSessions: make(map[string]*Session),
+		store:     &HeaderStore{KeyName: keyName},
+	}
+	mgr.GC()
+	return mgr
 }
 
 func NewSessionMgr(cookieName string, maxTime int64) *SessionMgr {
 	mgr := &SessionMgr{
-		mCookieName: cookieName,
-		mMaxTime:    maxTime,
-		mSessions:   make(map[string]*Session),
+		mKeyName:  cookieName,
+		mMaxTime:  maxTime,
+		mSessions: make(map[string]*Session),
 	}
 	mgr.GC()
 	return mgr
@@ -31,7 +54,7 @@ func generCookieValue() string {
 	bytes := []byte(str)
 	result := []byte{}
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	for i := 0; i < COOKIE_LENGTH; i++ {
+	for i := 0; i < KEY_LENGTH; i++ {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
@@ -52,10 +75,10 @@ func (p *SessionMgr) GC() {
 }
 
 func (p *SessionMgr) Set(r *http.Request, w http.ResponseWriter, key, val interface{}) {
-	cookie, err := r.Cookie(p.mCookieName)
+	cookie, err := r.Cookie(p.mKeyName)
 	if err != nil {
 		cookie = &http.Cookie{
-			Name:    p.mCookieName,
+			Name:    p.mKeyName,
 			Value:   generCookieValue(),
 			Expires: time.Now().Add(time.Duration(p.mMaxTime)),
 		}
@@ -75,7 +98,7 @@ func (p *SessionMgr) Set(r *http.Request, w http.ResponseWriter, key, val interf
 }
 
 func (p *SessionMgr) Get(r *http.Request, key interface{}) (interface{}, bool) {
-	cookie, err := r.Cookie(p.mCookieName)
+	cookie, err := r.Cookie(p.mKeyName)
 	if err != nil {
 		return nil, false
 	}
@@ -93,7 +116,7 @@ func (p *SessionMgr) Get(r *http.Request, key interface{}) (interface{}, bool) {
 }
 
 func (p *SessionMgr) Del(r *http.Request, w http.ResponseWriter) {
-	cookie, err := r.Cookie(p.mCookieName)
+	cookie, err := r.Cookie(p.mKeyName)
 	if err != nil {
 		return
 	}
---
+++ store.go
@@ -0,0 +1,45 @@
+package session
+
+import (
+	"net/http"
+	"time"
+)
+
+type Store interface {
+	Get(r *http.Request) string
+	Set(w http.Writer)
+}
+
+type CookieStore struct {
+	ExpTime int64
+	KeyName string
+}
+
+func (p *CookieStore) Get(r *http.Request) string {
+	cookie, err := r.Cookie(p.KeyName)
+	if err != nil {
+		return ""
+	}
+	return cookie.Value
+}
+
+func (p *CookieStore) Set(w http.Writer, val string) {
+	cookie := &http.Cookie{
+		Name:    p.KeyName,
+		Value:   val,
+		Expires: time.Now().Add(time.Duration(p.ExpTime)),
+	}
+	http.SetCookie(w, cookie)
+}
+
+type HeaderStore struct {
+	KeyName string
+}
+
+func (p *HeaderStore) Get(r *http.Request) string {
+	return r.Header.Get(p.KeyName)
+}
+
+func (p *HeaderStore) Set(w http.Writer, val string) {
+	w.Header().Set(p.KeyName, val)
+}
